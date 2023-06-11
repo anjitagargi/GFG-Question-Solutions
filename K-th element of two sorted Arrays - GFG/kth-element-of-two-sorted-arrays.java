4bb7b72fbac7fbe9d75dff76ae29119bc53d9b1d
Anjita Gargi Chandora <91597725+anjitagargi@users.noreply.github.com>
2023-06-11 22:30:38 +0530

    Added solution - LeetHub

---
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            int k = Integer.parseInt(stt.nextToken());
+            int a[] = new int[(int)(n)];
+            int b[] = new int[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            System.out.println(obj.kthElement( a, b, n, m, k));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        
+        int sorted[] = new int[m+n];
+        for(int i=0; i<n ; i++ ){
+            sorted[i]= arr1[i];
+            
+        }
+        
+        for(int i=n; i<m+n ; i++ ){
+            sorted[i]= arr2[i-n];
+            
+        }
+        
+        Arrays.sort(sorted);
+        
+        return sorted[k-1];
+        
+        
+    }
+}

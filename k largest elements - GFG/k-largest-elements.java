144c35a3de14e2223afd2f834ef8dd366daa6733
Anjita Gargi Chandora <91597725+anjitagargi@users.noreply.github.com>
2023-06-13 14:40:22 +0530

    Added solution - LeetHub

---
+++ k largest elements - GFG/k-largest-elements.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int[] kLargest(int[] arr, int n, int k) {
+        
+         Arrays.sort(arr);
+      int[] lastKElements = new int[k];
+         int start = arr.length - 1;
+    if (start < 0) {
+        start = 0; 
+    }
+         int index = 0;
+
+    for (int i = start; i > n-k-1; i--) {
+        lastKElements[index] = arr[i];
+        index++;
+    }
+    
+    return lastKElements;
+    }
+}

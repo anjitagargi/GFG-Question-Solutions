f28527d5b15502835c83d0f2dff484985d005384
Anjita Gargi Chandora <91597725+anjitagargi@users.noreply.github.com>
2023-07-18 01:04:23 +0530

    Added solution - LeetHub

---
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String str = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.LongestRepeatingSubsequence(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int LongestRepeatingSubsequence(String str)
+    {
+        // code here
+        int n = str.length();
+  
+        // Create and initialize DP table
+        int[][] dp = new int[n+1][n+1];
+  
+        // Fill dp table (similar to LCS loops)
+        for (int i=1; i<=n; i++)
+        {
+            for (int j=1; j<=n; j++)
+            {
+                // If characters match and indexes are not same
+                if (str.charAt(i-1) == str.charAt(j-1) && i!=j)
+                    dp[i][j] =  1 + dp[i-1][j-1];         
+                       
+                // If characters do not match
+                else
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+         return dp[n][n];
+    }
+}

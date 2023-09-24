f4596398514e923b2ab2e7e61b09970cf77a6e2b
Anjita Gargi Chandora <91597725+anjitagargi@users.noreply.github.com>
2023-09-24 22:41:54 +0530

    Added solution - LeetHub

--- Find duplicates in an array - GFG/find-duplicates-in-an-array.java
+++ Find duplicates in an array - GFG/find-duplicates-in-an-array.java
@@ -24,62 +24,25 @@ class GFG {
 
 class Solution {
     public static ArrayList<Integer> duplicates(int arr[], int n) {
-        	// Increment array elements by 1
-	for (int i = 0; i < n; i++) {
-	arr[i] += 1;
-	}
-	
-	
-	// result list
-	ArrayList<Integer> res = new ArrayList<>();
-
-	// count variable for count of
-	// largest element
-	int count = 0;
-
-	for (int i = 0; i < n; i++) {
-
-	// Calculate index value
-	int index = Math.abs(arr[i]) > n
-		? Math.abs(arr[i]) / (n + 1)
-		: Math.abs(arr[i]);
-
-	// Check if index equals largest element value
-	if (index == n) {
-		count++;
-		continue;
-	}
-
-	// Get element value at index
-	int val = arr[index];
-
-	// Check if element value is negative, positive
-	// or greater than n
-	if (val < 0) {
-		res.add(index - 1);
-		arr[index] = Math.abs(arr[index]) * (n + 1);
-	}
-	else if (val > n)
-		continue;
-	else
-		arr[index] = -arr[index];
-	}
-
-	// If largest element occurs more than once
-	if (count > 1)
-	res.add(n - 1);
-
-	if (res.size() == 0)
-	res.add(-1);
-	else
-	Collections.sort(res);
-
-	return res;
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            int index = arr[i] % n;
+            arr[index] += n;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if ((arr[i] / n) > 1) {
+                result.add(i);
+            }
+        }
+        
+        if (result.isEmpty()) {
+            result.add(-1);
+        } else {
+            Collections.sort(result);
+        }
+        
+        return result;
     }
 }
-
-
-
-
-
-
